d6a3c40999a0cefed81b6d751b341771e85b6ed5
Bibek Yadav <bibekyadav00@gmail.com>
2025-04-18 20:03:48 +0530

    visit

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,10 @@ export default function Home() {
                  Catered by: Delicious Kitchen
                 </h1>
                 <p className="text-lg text-white/80">
-                  <span className="font-semibold">Open for all the students</span> at NIT Durgapur
+                  <span className="font-semibold">
+                </p>
+                <p className="text-lg text-white/80">
+                  <span className="font-semibold">Visit the canteen today, open for all the students</span> 
                 </p>
                
                 <div className="flex flex-col sm:flex-row gap-4">
