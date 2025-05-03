6b465ec0c2d9846a534c4262b2ff9449f7b99597
Bibek Yadav <bibekyadav00@gmail.com>
2025-05-03 12:09:19 +0530

    cb

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -167,7 +167,7 @@ const currentmenu = [
   {
     name: "DINNER",
     items: [     
-      { name: "Chicken Biryani (1pc chicken,1pc aloo)", price: "80" },
+      { name: "Chicken Biryani @SATURDAY night (1pc chicken,1pc aloo)", price: "80" },
       { name: "Chicken Bharta (HALF 2pcs/FULL 4pcs)", price: "50/90" },
       { name: "Chicken Kasa (HALF 2pcs/FULL 4pcs)", price: "50/90" },
       { name: "Butter Chicken (HALF 2pcs/FULL 4pcs)", price: "50/90" },
